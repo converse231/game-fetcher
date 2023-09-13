f697946ee1ad31fd6ac5deb6198bef0c0df96a14
converse231 <paragasdaniel23@gmail.com>
2023-09-13 19:42:27 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-300">
+        Hello world! This is a game database
+      </h1>
+    </div>
+  );
+}
+
+export default App;
